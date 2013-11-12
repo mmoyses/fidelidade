10faf655baa13c0765160a0e11ef99ffb210b5ad
Marcus Moyses <marcus.moyses@gmail.com>
2013-11-12 20:29:02 -0200

    auth

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -1,24 +1,7 @@
 exports.requiresLogin = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.send(401, 'Usuário não autorizado');
-  }
-  next();
-};
-
-exports.user = {
-  hasAuthorization: function(req, res, next) {
-    if (req.profile.id !== req.user.id) {
-      return res.send(401, 'Usuário não autorizado');
-    }
+  if (req.isAuthenticated()) {
     next();
-  }
-};
-
-exports.article = {
-  hasAuthorization: function(req, res, next) {
-    if (req.article.user.id !== req.user.id) {
-      return res.send(401, 'Usuário não autorizado');
-    }
-    next();
+  } else {
+    res.redirect('/entrar');
   }
 };
--- config/routes.js
+++ config/routes.js
@@ -1,24 +1,19 @@
 module.exports = function(app, passport) {
   var usuarios = require('../app/controllers/usuarios'),
-    empresas = require('../app/controllers/empresas');
+    empresas = require('../app/controllers/empresas'),
+    auth = require('./middlewares/authorization');
   app.get('/entrar', usuarios.signin);
   app.get('/cadastrar', usuarios.signup);
   app.get('/sair', usuarios.signout);
 
   app.post('/usuarios', usuarios.create);
-
+  app.get('/usuarios/:userId', usuarios.show);
   app.post('/usuarios/session', passport.authenticate('local', {
     failureRedirect: '/entrar',
     failureFlash: 'Email ou senha inválida.'
   }), usuarios.session);
 
-  app.get('/usuarios/eu', usuarios.me);
-  app.get('/usuarios/:userId', usuarios.show);
-
-  //Finish with setting up the userId param
-  app.param('userId', usuarios.user);
-
-  app.get('/empresa', empresas.new);
+  app.get('/empresa', auth.requiresLogin ,empresas.new);
   app.post('/empresas', empresas.create);
   app.get('/empresa/:companyId', empresas.edit);
   app.post('/empresa/:companyId', empresas.editAction);
