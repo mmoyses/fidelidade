772ebfb965eadc7247ac7cafb951e77e254bec9b
Marcus Moyses <marcus.moyses@gmail.com>
2013-12-01 13:42:57 -0800

    adding grunt-concat

--- gruntfile.js
+++ gruntfile.js
@@ -1,96 +1,106 @@
 module.exports = function(grunt) {
-    // Project Configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            jade: {
-                files: ['app/views/**'],
-                options: {
-                    livereload: true
-                }
-            },
-            js: {
-                files: ['public/js/**', 'app/**/*.js'],
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            html: {
-                files: ['public/views/**'],
-                options: {
-                    livereload: true
-                }
-            },
-            css: {
-                files: ['public/css/**'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        jshint: {
-            all: ['gruntfile.js', 'public/js/**/spa.*.js', 'test/**/*.js', 'app/**/*.js']
-        },
-        nodemon: {
-            dev: {
-                options: {
-                    file: 'server.js',
-                    args: [],
-                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
-                    watchedExtensions: ['js'],
-                    watchedFolders: ['app', 'config'],
-                    debug: true,
-                    delayTime: 1,
-                    env: {
-                        PORT: 3000
-                    },
-                    cwd: __dirname
-                }
-            }
-        },
-        concurrent: {
-            tasks: ['nodemon', 'watch'],
-            options: {
-                logConcurrentOutput: true
-            }
-        },
-        mochaTest: {
-            options: {
-                reporter: 'spec'
-            },
-            src: ['test/**/*.js']
-        },
-        bower: {
-            install: {
-                options: {
-                    targetDir: './public/lib',
-                    layout: 'byComponent',
-                    install: true,
-                    verbose: true,
-                    cleanBowerDir: true
-                }
-            }
+  // Project Configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      jade: {
+        files: ['app/views/**'],
+        options: {
+          livereload: true
         }
-    });
+      },
+      js: {
+        files: ['public/js/**', 'app/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ['public/views/**'],
+        options: {
+          livereload: true
+        }
+      },
+      css: {
+        files: ['public/css/**'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    jshint: {
+      all: ['gruntfile.js', 'app/**/*.js', 'test/**/*.js']
+    },
+    nodemon: {
+      dev: {
+        options: {
+          file: 'server.js',
+          args: [],
+          ignoredFiles: ['README.md', 'node_modules/**'],
+          watchedExtensions: ['js'],
+          watchedFolders: ['app', 'config'],
+          debug: true,
+          delayTime: 1,
+          env: {
+            PORT: 3000
+          },
+          cwd: __dirname
+        }
+      }
+    },
+    concurrent: {
+      tasks: ['nodemon', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
+    },
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      src: ['test/**/*.js']
+    },
+    bower: {
+      install: {
+        options: {
+          targetDir: './public/lib',
+          layout: 'byComponent',
+          install: true,
+          verbose: true,
+          cleanBowerDir: true
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['app/frontend/spa.js', 'app/frontend/spa.util.js', 'app/frontend/spa.data.js',
+              'app/frontend/spa.fake.js', 'app/frontend/spa.model.js', 'app/frontend/spa.home.js',
+              'app/frontend/spa.checkin.js', 'app/frontend/spa.checkout.js', 'app/frontend/spa.consultar.js',
+              'app/frontend/spa.relatorios.js', 'app/frontend/spa.user.js', 'app/frontend/spa.shell.js'],
+        dest: 'public/js/spa.js'
+      }
+    }
+  });
 
-    //Load NPM tasks
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-nodemon');
-    grunt.loadNpmTasks('grunt-concurrent');
-    grunt.loadNpmTasks('grunt-bower-task');
+  //Load NPM tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-    //Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
+  //Making grunt default to force in order not to break the project.
+  grunt.option('force', true);
 
-    //Default task(s).
-    grunt.registerTask('default', ['jshint', 'concurrent']);
+  //Default task(s).
+  grunt.registerTask('default', ['jshint', 'concat', 'concurrent']);
 
-    //Test task.
-    grunt.registerTask('test', ['mochaTest']);
+  //Test task.
+  grunt.registerTask('test', ['mochaTest']);
 
-    //Bower task.
-    grunt.registerTask('install', ['bower']);
+  //Bower task.
+  grunt.registerTask('install', ['bower']);
 };
